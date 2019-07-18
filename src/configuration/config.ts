fe2180bc3717a953fb3637cf064711254bf631f7
EmidioVinicius <vinicius.s.emidio@gmail.com>
2019-07-18 18:04:08 -0300

    initializethe ruotes

---
+++ src/configuration/config.ts
@@ -0,0 +1,3 @@
+export default{ // exporta todo o objeto
+    port: process.env.PORT || 3000 //Procura PORT como variavel de ambiente, se nao encontrar usa 3000
+} 
---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import "reflect-metadata";
+import {createConnection} from "typeorm";
+import * as express from "express";
+import * as bodyParser from "body-parser";
+import {Request, Response} from "express";
+import {Routes} from "./routes";
+import config from "./configuration/config";
+
+
+
+    
+    const app = express();
+    app.use(bodyParser.json());
+
+    
+    Routes.forEach(route => {
+        (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
+            const result = (new (route.controller as any))[route.action](req, res, next);
+            if (result instanceof Promise) {
+                result.then(result => result !== null && result !== undefined ? res.send(result) : undefined);
+
+            } else if (result !== null && result !== undefined) {
+                res.json(result);
+            }
+        });
+    });
+
+    app.listen(config.port, '0.0.0.0', async () =>{
+        try {
+            await createConnection()
+            console.log('Database connected')
+        } catch (error) {
+            console.error("Database not cnnectes", error)
+        }
+    })
